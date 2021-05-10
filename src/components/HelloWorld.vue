f30d65021a6331463ffe80a546f1428d49eae893
tipkip9tok <ivanscov@rambler.ru>
2021-05-10 20:56:39 +0300

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,123 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="wrapper">
+    <h1>Прогноз погоды</h1>
+    <div class="pressure">
+      Максимальное давление за последние 5 дней:{{ maxPressure }}
+    </div>
+    <div>
+      <button @click="loadPressure" class="btn">Расчёт давления</button>
+    </div>
+    <div class="temperature">
+      День с минимальной разницей температур:{{ realDay }}
+    </div>
+    <div>
+      <button @click="loadTemp" class="btn">Расчёт температуры</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      date: (Date.now() / 1000 - 86400).toFixed(0),
+      lat: 60.99,
+      lon: 30.9,
+      unixDay: 86400,
+      realDay: 0,
+      maxPressure: 0,
+    };
+  },
+  methods: {
+    async loadPressure() {
+      let array = [];
+      const instance = axios.create({
+        baseURL: "http://api.openweathermap.org/data/2.5/onecall",
+        params: {
+          appid: "21e9bc9c7139e76a6bc9e3a6a63860df",
+          lat: this.lat,
+          lon: this.lon,
+        },
+      });
+      axios
+        .all([
+          await instance.get("/timemachine", { 
+            params: { dt: this.date } 
+          }),
+          await instance.get("/timemachine", {
+            params: { dt: this.date - 1 * this.unixDay }
+          }),
+          await instance.get("/timemachine", {
+            params: { dt: this.date - 2 * this.unixDay }
+          }),
+          await instance.get("/timemachine", {
+            params: { dt: this.date - 3 * this.unixDay }
+          }),
+          await instance.get("/timemachine", {
+            params: { dt: this.date - 4 * this.unixDay }
+          })
+        ])
+        .then((response) => {
+          for (let i = 0; i < response.length; i++) {
+            for (let j = 0; j < response[i].data.hourly.length; j++) {
+              array.push(response[i].data.hourly[j].pressure);
+            }
+          }
+          this.maxPressure = Math.max.apply(null, array);
+        });
+    },
+    async loadTemp() {
+      let arr = [];
+      await axios
+        .get("http://api.openweathermap.org/data/2.5/onecall", {
+          params: {
+            appid: "21e9bc9c7139e76a6bc9e3a6a63860df",
+            exclude: "current,minutely,hourly,alerts",
+            lat: this.lat,
+            lon: this.lon,
+          },
+        })
+        .then((response) => {
+          for (let k = 0; k < response.data.daily.length; k++) {
+            arr.push(
+              Math.abs(
+                response.data.daily[k].temp.night -
+                  response.data.daily[k].temp.morn
+              ).toFixed(2)
+            );
+          }
+          let min = arr.indexOf(String(Math.min.apply(null, arr)));
+          let minDay = new Date(response.data.daily[min].dt * 1000);
+          this.realDay =
+            minDay.getDate() +
+            "/" +
+            (minDay.getMonth() + 1) +
+            "/" +
+            minDay.getFullYear();
+        });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+label {
+  padding-right: 30px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.btn {
+  margin: 10px 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+p {
+  font-size: 10px;
+  margin-top: 5px;
 }
+
 a {
   color: #42b983;
 }
